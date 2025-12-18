18753dc6fef4b6d59fa432bc16914bb8f6eaeee5
humeman <camdenjsf@gmail.com>
2025-12-17 18:47:53 -0600

    Init

---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "error: run this as root"
+    exit 1
+fi
+
+
+LOCATION=${LOCATION:-"/opt/ytdl-proxy"}
+PORT=${PORT:-8200}
+
+echo "Installing to ${LOCATION}"
+mkdir -p "${LOCATION}"
+
+echo "Getting yt-dlp"
+git -C "${LOCATION}/ytdl" clone https://github.com/yt-dlp/yt-dlp
+
+echo "Copying ytdl-proxy"
+cp -r . "${LOCATION}/ytdl-proxy"
+
+echo "Creating virtual environment"
+python3 -m venv "${LOCATION}/venv"
+
+echo "Installing requirements"
+old_dir=$(pwd)
+cd "${LOCATION}"
+source "venv/bin/activate"
+pip3 install -r ytdl-proxy/requirements.txt
+cd ytdl
+pip3 install .
+cd ..
+
+echo "Creating update service"
+
+cat <<EOF > /etc/systemd/system/ytdl-proxy-update.service
+[Unit]
+Description=Update ytdl-proxy
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=${LOCATION}/ytdl-proxy/update.sh
+EOF
+
+cat <<EOF > /etc/systemd/system/ytdl-proxy-update.timer
+[Unit]
+Description=Daily update for ytdl-proxy and yt-dlp
+
+[Timer]
+OnCalendar=*-*-* 00:00:00 UTC
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+echo "Creating start service"
+
+cat <<EOF > /etc/systemd/system/ytdl-proxy.service
+[Unit]
+Description=ytdl-proxy Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=${LOCATION}/ytdl-proxy/start.sh
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Enabling and starting services"
+systemctl daemon-reload
+systemctl enable --now ytdl-proxy-update.timer
+systemctl enable --now ytdl-proxy.service
+
+cd "$old_dir"
+echo "Done!"
